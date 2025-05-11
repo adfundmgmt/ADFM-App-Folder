6b4a7c9e44adf214b8ed2901fd58c3d022d087ba
adfundmgmt <aryadeniz@adfundmgmt.com>
2025-05-11 16:35:24 -0400

    Update 1_Monthly_Seasonality_Explorer.py

--- pages/1_Monthly_Seasonality_Explorer.py
+++ pages/1_Monthly_Seasonality_Explorer.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.set_page_config(page_title="Your Tool Name", layout="wide")
 
 ############################################################
 # Built by AD Fund Management LP.
