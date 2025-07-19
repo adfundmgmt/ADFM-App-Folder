a311c3d63dea1ed9cdb0d25dc6bf7bccb1563ee8
adfundmgmt <aryadeniz@adfundmgmt.com>
2025-07-19 04:10:22 -0400

    Update 6_Ratio_Charts.py

--- pages/6_Ratio_Charts.py
+++ pages/6_Ratio_Charts.py
@@ -36,9 +36,9 @@ with st.sidebar:
     )
 
     st.header("Look‑back")
-    spans = {"3 M": 90, "6 M": 180, "9 M": 270, "YTD": None,
-         "1 Y": 365, "3 Y": 365*3, "5 Y": 365*5}
-    default_ix = list(spans.keys()).index("5 Y")
+    spans = {"3 Months": 90, "6 Months": 180, "9 Months": 270, "YTD": None,
+         "1 Year": 365, "3 Year": 365*3, "5 Year": 365*5}
+    default_ix = list(spans.keys()).index("5 Year")
     span_key = st.selectbox("", list(spans.keys()), index=default_ix)
 
     st.markdown("---")
